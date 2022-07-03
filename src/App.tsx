50ff46960528acd303d9a5567bae8ea79d73e7cb
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro.local>
2022-07-03 00:01:17 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useLocalStorage from "use-local-storage";
+import Sqaure from './components/Square';
+import "./App.css";
 
 function App() {
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={theme}>
+      <h1>ml products</h1>
+      <button onClick={switchTheme}>
+        Switch to { theme === 'light' ? 'Dark' : 'Light' } theme
+      </button>
+      <Sqaure />
     </div>
   );
 }
---
+++ src/components/Square.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './square.css'
+
+const Square = () => {
+  return (
+    <div className="square">
+      <span>Square boi</span>
+    </div>
+  );
+};
+
+export default Square;
